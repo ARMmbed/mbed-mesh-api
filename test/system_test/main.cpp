196bfd0356a4bb3888d45dd4395f8f28af8042fc
Arto Kinnunen <arto.kinnunen@arm.com>
2015-10-06 12:43:33 +0300

    Update dependencies
    -update changed path also from test application.

--- test/system_test/main.cpp
+++ test/system_test/main.cpp
@@ -21,7 +21,7 @@
 #include "atmel-rf-driver/driverRFPhy.h"    // rf_device_register
 #include "test_cases.h"
 #include "mbed/test_env.h"
-#include "mbed-util/FunctionPointer.h"
+#include "core-util/FunctionPointer.h"
 #define HAVE_DEBUG 1
 #include "ns_trace.h"
 
